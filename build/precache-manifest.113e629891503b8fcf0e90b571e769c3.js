dfdf7526ff1eac10011480a729f2382af1f19a86
Chrisp <ultrachrisp@gmail.com>
2019-09-16 13:16:16 +0200

    Should be done with 3b.

---
+++ build/precache-manifest.113e629891503b8fcf0e90b571e769c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124ff73ea2b1a5c9f7fa1c7932ce649c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0514d056da2f9ee549",
+    "url": "/static/css/main.7f22e76d.chunk.css"
+  },
+  {
+    "revision": "0d0ad37291ca8eb8e46e",
+    "url": "/static/js/2.926fd93e.chunk.js"
+  },
+  {
+    "revision": "0b0514d056da2f9ee549",
+    "url": "/static/js/main.5c3abba4.chunk.js"
+  },
+  {
+    "revision": "de87ddd604b7d7b42500",
+    "url": "/static/js/runtime~main.5e5e2361.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 // app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('build'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 morgan.token('body', function(req, res) {
