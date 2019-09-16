0c907390a284f275221ccc60154d0b53847142c5
Chrisp <ultrachrisp@gmail.com>
2019-09-16 11:40:17 +0200

    Got morgan in place & working.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+morgan.token('body', function(req, res) {
+	return JSON.stringify({name: req.body.name, number:req.body.number});
+});
 
 let phonebook = {
     "persons": [
@@ -28,29 +34,6 @@ let phonebook = {
         }
     ]
 };
-
-/*    "persons": [
-        {
-            "name": "Arto Hellas",
-            "number": "040-123456",
-            "id": 1
-        },
-        {
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523",
-            "id": 2
-        },
-        {
-            "name": "Dan Abramov",
-            "number": "12-43-234345",
-            "id": 3
-        },
-        {
-            "name": "Mary Poppendieck",
-            "number": "39-23-6423122",
-            "id": 4
-        }
-        ]*/
 
 function generateID(){
     return Math.floor(Math.random()* 99999);
