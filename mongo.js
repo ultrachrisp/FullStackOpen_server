6f3befb46cb40b18903d387df26b6c1b8b90879d
Chrisp <ultrachrisp@gmail.com>
2019-09-20 09:47:52 +0200

    Fixing merge issues.

--- mongo.js
+++ mongo.js
@@ -7,8 +7,6 @@ if ( process.argv.length<3 ) {
 
 const password = process.argv[2];
 
-// const url = `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`;
-<<<<<<< HEAD
 const url = `mongodb+srv://goTime:${password}@cluster0-ax72n.mongodb.net/phonebook-app?retryWrites=true&w=majority`;
 
 mongoose.connect(url, { useNewUrlParser: true });
@@ -38,35 +36,4 @@ if(process.argv[3] && process.argv[4]){
         mongoose.connection.close();
     });
 }
-=======
-const url = `mongodb+srv://goTime:${password}@cluster0-ax72n.mongodb.net/note-app?retryWrites=true&w=majority`;
-
-mongoose.connect(url, { useNewUrlParser: true });
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-});
-
-const Note = mongoose.model('Note', noteSchema);
-
-// const note = new Note({
-//   content: 'HTML is Easy',
-//   date: new Date(),
-//   important: true,
-// });
-
-// note.save().then(response => {
-//     console.log('note saved!');
-//     mongoose.connection.close();
-// });
-
-Note.find({}).then(result => {
-  result.forEach(note => {
-      console.log(note);
-  });
-    mongoose.connection.close();
-});
->>>>>>> c3c8a8ba7c234f0eb3eba7d8b456c463adfde88b
 
